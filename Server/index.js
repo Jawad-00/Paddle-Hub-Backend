34dc0214732ea87a92cf6995746f53ec3cc724dc
ali-097 <aalimehmoodd@gmail.com>
2025-09-04 19:19:55 +0500

    Added CORS handling to server

--- Server/index.js
+++ Server/index.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/auth.routes');
-const courtRoutes = require('./routes/court.routes');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/auth.routes");
+const courtRoutes = require("./routes/court.routes");
+const cors = require("cors");
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/courts', courtRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/courts", courtRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Padel Hub API');
+app.get("/", (req, res) => {
+  res.send("Welcome to Padel Hub API");
 });
 
 const PORT = process.env.PORT || 5000;
