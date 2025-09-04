82f032996fc01828f9bd513fa65f4002db40a2a2
Jawad Bin Hamid <jawadbinhamid@gmail.com>
2025-09-04 09:09:46 -0700

    image upload via cloudinary integrated

--- Server/routes/court.routes.js
+++ Server/routes/court.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const CourtController = require('../controllers/court.controller');
 const auth = require('../middleware/auth.middleware');
+const upload = require('../middleware/upload.middleware');
 
 const router = express.Router();
 
-router.post('/', auth('admin'), CourtController.createCourt);
-router.put('/:id', auth('admin'), CourtController.updateCourt);
+router.post('/', auth('admin'), upload.single('image'), CourtController.createCourt);
+router.put('/:id', auth('admin'), upload.single('image'), CourtController.updateCourt);
 router.delete('/:id', auth('admin'), CourtController.deleteCourt);
 router.post('/:id/closed-date', auth('admin'), CourtController.addClosedDate);
 
