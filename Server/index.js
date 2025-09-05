63ce4e5beb56558bc9267e9ab5e2438f8d07a82b
Jawad Bin Hamid <jawadbinhamid@gmail.com>
2025-09-05 05:30:24 -0700

    index.js updated

--- Server/index.js
+++ Server/index.js
@@ -3,21 +3,19 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth.routes');
 const courtRoutes = require('./routes/court.routes');
-const cors = require('cors');
+const bookingRoutes = require('./routes/booking.routes');
+
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/courts', courtRoutes);
+app.use('/auth', authRoutes);
+app.use('/courts', courtRoutes);
+app.use('/bookings', bookingRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Padel Hub API');
-});
+app.get('/', (req, res) => res.send('Welcome to Padel Hub API'));
 
 const PORT = process.env.PORT || 5000;
-
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
